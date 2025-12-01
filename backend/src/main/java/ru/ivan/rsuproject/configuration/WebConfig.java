19b5bade2601b09f0924bc4630371d16c819a73a
ivan <ivan-osokin1980@yandex.ru>
2025-12-01 11:02:01 +0300

    fix: fix cors config for frontend

--- backend/src/main/java/ru/ivan/rsuproject/configuration/WebConfig.java
+++ backend/src/main/java/ru/ivan/rsuproject/configuration/WebConfig.java
@@ -1,21 +1,18 @@
 package ru.ivan.rsuproject.configuration;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
-                        .allowedMethods("*");
-            }
-        };
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
---
+++ backend/src/test/java/ru/ivan/rsuproject/RsUprojectApplicationTests.java
@@ -0,0 +1,5 @@
+package ru.ivan.rsuproject;
+
+public class RsUprojectApplicationTests {
+
+}
